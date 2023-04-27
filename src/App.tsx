7804b7f99853ea86e22f38f94f86ffaf709ae717
tsremarkable <tsformable@gmail.com>
2023-04-27 12:13:14 -0400

    feat: main-features but not outlined in JD

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,42 @@ import logo from "./logo.svg";
 import "./App.css";
 import Title from "./Title/title";
 import { ReactDOM } from "react";
-import { Editor, EditorState } from "draft-js";
+import { convertToRaw, Editor, EditorState, RichUtils, DraftInlineStyle } from "draft-js";
 import "draft-js/dist/Draft.css";
+import InlineStyleControls from "./InlineStylesControls";
 
 function App() {
     const [editorState, setEditorState] = useState(() =>
         EditorState.createEmpty()
     );
+
+
+    const handleEditorChange = (newState: any) => {
+        setEditorState(newState);
+        const rawContentState = convertToRaw(newState.getCurrentContent());
+        const text = rawContentState.blocks[0].text;
+        console.log(text);
+    }
     const editor = useRef<any>(null);
     const focusEditor = () => {
         editor.current?.focus();
     };
+
+    const customStyleMap = {
+      STRIKETHROUGH: {
+        textDecoration: "line-through"
+      },
+      FONT_SIZE_30: {
+        fontSize: "30px"
+      }
+    };
+
+    const toggleInlineStyle = (inlineStyle: string) => {
+      const newState = RichUtils.toggleInlineStyle(editorState, inlineStyle);
+      const newState2 = RichUtils.toggleBlockType(newState, "header-one");
+      setEditorState(newState2);
+    };
+
     return (
         <div className="App">
             <Title />
@@ -25,10 +50,16 @@ function App() {
                 }}
                 onClick={focusEditor}
             >
+              <InlineStyleControls
+                currentInlineStyle={editorState.getCurrentInlineStyle()}
+                onToggle={toggleInlineStyle}
+              />
+
                 <Editor
                     ref={editor}
+                    customStyleMap={customStyleMap}
                     editorState={editorState}
-                    onChange={setEditorState}
+                    onChange={handleEditorChange}
                     placeholder="Write something!"
                 />
             </div>
---
+++ src/InlineStylesControls.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export const inlineStyles = [
+  { label: "B", desc: "font-bold", style: "BOLD" },
+  { label: "I", desc: "italic", style: "ITALIC" },
+  { label: "U", desc: "underline", style: "UNDERLINE" },
+  { label: "S", desc: "line-through", style: "STRIKETHROUGH" },
+  { label: "H1", style: "FONT_SIZE_30", desc:  "heading" }
+];
+
+
+interface InlineStyleControlsProps {
+      currentInlineStyle: any;
+      onToggle: (inlineStyle: string) => void;
+    }
+
+const InlineStyleControls = ({ currentInlineStyle, onToggle }:  InlineStyleControlsProps) => {
+  return (
+    <div>
+      {inlineStyles.map(type => (
+        <button
+          key={type.label}
+          onMouseDown={e => {
+            e.preventDefault();
+            onToggle(type.style);
+          }}
+          className="border-[1px] pl-2 pr-2 mr-3 border-blue-500 hover:bg-blue-500 visited:bg-blue-300"
+        >
+            {<p className={type.desc}>{type.label}</p>}
+
+          
+
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default InlineStyleControls;
